48c715367d69b20c113c7589e9f1beff287a60f6
isacandrei <isacandrei19@gmail.com>
2016-11-11 00:22:46 +0100

    AWS integration OK

--- Vagrant_AWS/addSecurityRule.sh
+++ Vagrant_AWS/addSecurityRule.sh
@@ -2,4 +2,4 @@
 # addSecurityRule.sh
 [ -r lastIP ] && [ -r removeSecurityRule.sh ] && ./removeSecurityRule.sh
 currentIP=`curl -s http://ipinfo.io/ip`
-aws ec2 authorize-security-group-ingress --group-id sg-4d175d25 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 22, \"ToPort\": 22, \"IpRanges\": [{\"CidrIp\": \"$currentIP/32\"}]}]" && echo $currentIP > lastIP
+aws ec2 authorize-security-group-ingress --group-id sg-862f65ee --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 22, \"ToPort\": 22, \"IpRanges\": [{\"CidrIp\": \"$currentIP/32\"}]}]" && echo $currentIP > lastIP
--- Vagrant_AWS/nginx.sh
+++ Vagrant_AWS/nginx.sh
@@ -2,7 +2,8 @@ sudo su
 apt-get update
 apt -y install docker.io
 service docker start
-export DOCKER_IP='curl -s http://ipinfo.io/ip'
-docker run -d --net=host --name consul consul agent -server -bind=$DOCKER_IP -bootstrap -client=$DOCKER_IP -ui
+export DOCKER_IP='172.31.25.5'
+export CLIENT_IP='curl -s http://ipinfo.io/ip'
+docker run -d --net=host --name consul consul agent -server -bind=$DOCKER_IP -bootstrap -client=$DOCKER_IP -ui consul
 docker run -d --name nginx -p 80:80 isac/nginx-consul-template:1.2
 
